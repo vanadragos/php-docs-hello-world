0476367e7f27ab6f6f03edc19bbb4e14a0cf8f0f
vanadragos <vanadragos@gmail.com>
2022-09-26 19:04:55 +0300

    step 6

--- index.php
+++ index.php
@@ -1,9 +1,29 @@
 <?php
 
-echo "Hello World 127!";
+echo "Hello World get!";
 
+$connectstr_dbhost = '';
+$connectstr_dbname = '';
+$connectstr_dbusername = '';
+$connectstr_dbpassword = '';
 
+foreach ($_SERVER as $key => $value) {
+    if (strpos($key, "MYSQLCONNSTR_localdb") !== 0) {
+        continue;
+    }
 
+    $connectstr_dbhost = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $value);
+    $connectstr_dbname = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $value);
+    $connectstr_dbusername = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
+    $connectstr_dbpassword = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
+}
+
+print $connectstr_dbhost.'<br>';
+print $connectstr_dbname.'<br>';
+print $connectstr_dbusername.'<br>';
+print $connectstr_dbpassword.'<br>';
+
+exit();
 
 //connection
 $user = "azure";
